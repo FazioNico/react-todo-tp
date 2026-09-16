fa7365d98e4df8c7db56a4a0e7f03f5d56d41d03
FazioNico <contact@nicolasfazio.ch>
2026-09-16 11:52:32 +0200

    feat: implement Google authentication in App component

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import { TodoForm } from './components/TodoForm'
 import { TodosList } from './components/TodosList'
+import { signinWithGoogle } from './services/firebase/firebase'
 
 function App() {
 
   return (
     <>
+      <button onClick={()=> signinWithGoogle()}>auth with Google</button>
       <TodoForm />
       <TodosList />
     </>
--- src/services/firebase/firebase.js
+++ src/services/firebase/firebase.js
@@ -2,6 +2,7 @@
 import { initializeApp } from "firebase/app";
 import { firebaseConfig } from "./firebase.config";
 import { getDatabase, push, ref } from "firebase/database";
+import { getAuth, GoogleAuthProvider, signInWithPopup } from "firebase/auth";
 
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
@@ -9,6 +10,7 @@ import { getDatabase, push, ref } from "firebase/database";
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 export const database = getDatabase(app);
+export const auth = getAuth(app);
 
 export const collection = ref(database, 'tp-todos-react');
 
@@ -17,4 +19,10 @@ export const addToFirebase = async (title) => {
     title,
     createAt: new Date().toISOString()
   });
+}
+
+export const signinWithGoogle = async () => {
+  const provider = new GoogleAuthProvider();
+  const userCredential = await signInWithPopup(auth, provider);
+  return userCredential.user;
 }
